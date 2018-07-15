9dbb0c8dc99a91f4f5398f4d04fce869c518e181
Harman Kaur <timahrmn@gmail.com>
2018-07-15 12:04:18 -0700

    re 4.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :object_names
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/products'
   get 'simple_pages/faq'
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'simple_pages#landing_page'
-resources :orders, only: [:index, :show, :create, :destroy]
+
 end
