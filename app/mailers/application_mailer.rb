7f7f3215320828c251eba7898ee75b56420c4cf5
Harman Kaur <timahrmn@gmail.com>
2018-07-18 15:13:14 -0700

    5.3

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'timahrmn@gmail.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ resources :users, except: [:index]
   get 'simple_pages/orders'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'simple_pages#landing_page'
 
