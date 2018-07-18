5d3786b21c87388d750f318a2d6117e77b9c0d54
Harman Kaur <timahrmn@gmail.com>
2018-07-18 15:25:40 -0700

    7

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -9,6 +9,9 @@ def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver_now
+  ActionMailer::Base.mail(from: @email,
+      to: 'timahrmn@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
 end
 end
