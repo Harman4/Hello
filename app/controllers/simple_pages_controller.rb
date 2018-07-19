47cae95f32cb102704b8a76b7cbdc72378236c84
Harman Kaur <timahrmn@gmail.com>
2018-07-18 23:28:03 -0700

    5.3

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,15 +3,13 @@ class SimplePagesController < ApplicationController
   end
 
 def landing_page
+  @featured_product = Product.first
   @products = Product.limit(3)
 end
 def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'timahrmn@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+   UserMailer.contact_form(@email, @name, @message).deliver_now
 end
 end
