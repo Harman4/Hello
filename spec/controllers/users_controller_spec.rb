65e27ea555d51eacd40391ead1713a19fe4c614d
Harman Kaur <timahrmn@gmail.com>
2018-07-28 18:55:47 -0700

    6.3

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+  let(:user) { User.create!(email: "foobar@gmai.com",
+    password: "nonene")}
+
+    let(:user1) { User.create!(email: "1foobar@gmai.com",
+      password: "1nonene")}
+    describe 'GET #show' do
+
+      context 'when a user is logged in' do
+
+        before do
+          sign_in user
+        end
+
+        it 'loads correct user details' do
+          get :show, params: { id: user.id }
+          expect(response).to be_ok
+          expect(assigns(:user)).to eq user
+        end
+      end
+
+      context 'when a user is not logged in' do
+        it 'redirects to login' do
+          get :show, params: { id: user.id }
+          expect(response).to redirect_to(new_user_session_path)
+
+        end
+      end
+    end
+  end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,7 +35,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  # For Devise
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -54,4 +56,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+require_relative 'support/controller_macros' # or require_relative '../controller_macros' if write in `spec/support/devise.rb`
+
+RSpec.configure do |config|
+  # For Devise > 4.1.1
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  # Use the following instead if you are on Devise <= 4.1.1
+  # config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
