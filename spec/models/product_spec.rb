863b1bd05bf0266fdf970c51f68725771005a124
Harman Kaur <timahrmn@gmail.com>
2018-07-28 17:46:49 -0700

    6.2

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,6 +9,7 @@ describe Product do
 
   end
   it "returns the average rating of all comments" do
+    expect(product.average_rating). to eq 3.0
   end
   it "is not valid without a name" do
     expect(Product.new(description: "Nice bike")).not_to be_valid
