773070da892def5a9be3833ccb80a19ab3ec896a
Harman Kaur <timahrmn@gmail.com>
2018-07-30 20:08:59 -0700

    6.6

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,25 @@
+class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:product_id])
+    @user = current_user
+
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@product.price * 100).to_i,
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail],
+        :receipt_email => params[:stripeEmail]
+      )
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price * 100)
+      end
+      redirect_to product_path(@product)
+    rescue Stripe::CardError => e
+      # The card has been declined
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,19 @@ Rails.application.routes.draw do
   resources :object_names
 
   resources :orders, only: [:index, :show, :create, :destroy]
-resources :users, except: [:index]
-resources :products do
-  resources :comments
-end
-resources :users
+  resources :users, except: [:index]
+  resources :products do
+    resources :comments
+  end
+  resources :users
   get 'simple_pages/about'
   get 'simple_pages/products'
   get 'simple_pages/orders'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   post 'simple_pages/thank_you'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'simple_pages#landing_page'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'simple_pages#landing_page'
+post 'payments/create'
 end
