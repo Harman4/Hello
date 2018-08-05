07e88962aa4f1758b83e78ac2f219784ebd62ab8
Harman Kaur <timahrmn@gmail.com>
2018-08-04 20:49:21 -0700

    6.14

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,5 +7,5 @@ validates :product, presence: true
 validates :rating, numericality: { only_integer: true }
 scope :rating_desc, -> { order(rating: :desc) }
 scope :rating_asc, -> {order(rating: :asc)}
-
+after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 end
